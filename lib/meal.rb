5c16b0d7cea71445aef0231e89522f21fb683b82
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-04 22:55:53 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,8 +2,14 @@ class Meal
   
   attr_reader :waiter, :customer, :total, :tip 
   
+  @@all = []
+  
   def initialize(waiter, customer, total, tip)
     @waiter, @customer, @total, @tip = waiter, customer, total, tip
+  end
+  
+  def self.all
+    @@all
   end
 
 
